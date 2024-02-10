c739eab7309b8d09f3fd5c63918d68835a4fcd04
CassieNovaCodes <159504701+CassieNovaCodes@users.noreply.github.com>
2024-02-10 10:29:04 -0800

    Update and rename paleLeafInstall.sh to paleLeafInstall_Part1.sh

--- paleLeafInstall_Part1.sh
+++ paleLeafInstall_Part1.sh
@@ -48,17 +48,7 @@ continue_with_installation() {
     clear
     echo "Mounting System"
     arch-chroot /mnt
-    clear
-    echo "Generating Locales"
-    hwclock --systohc
-    sed -i '/^# *en_US.UTF-8/s/^# *//' /etc/locale.gen
-    locale-gen
-    clear
-    echo "Setting Up Hostname"
-    echo "$hostname" | sudo tee /etc/hostname >/dev/null
-    clear
-    echo "Setting Passwords"
-    echo -e "$root_password\n$root_password" | passwd
+    
     
     
 }
