596453c058444dfc56e768dd7e1507e576f5cdca
CassieNovaCodes <159504701+CassieNovaCodes@users.noreply.github.com>
2024-02-10 12:56:36 -0800

    Update afterInstallTouchups.sh

--- afterInstallTouchups.sh
+++ afterInstallTouchups.sh
@@ -1,9 +1,9 @@
-sudo pacman -S xorg xorg-xinit xf86-video-fbdev picom nitrogen lxqt firefox --noconfirm
+sudo pacman -S xorg xorg-xinit xf86-video-fbdev picom nitrogen cinnamon firefox --noconfirm
 echo "\n" | pacman -S base-devel --noconfirm
 cd ~/
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 head -n -5 ~/.xinitrc > ~/.xinitrc.tmp && mv ~/.xinitrc.tmp ~/.xinitrc
 lines_to_add="picom&\n
 nitrogen&\n
-exec startlxqt"
+exec cinnamon-session"
 echo "$lines_to_add" >> ~/.xinitrc
