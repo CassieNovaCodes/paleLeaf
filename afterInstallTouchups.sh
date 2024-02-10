f31026460064837da2148d39483de33f4076811b
CassieNovaCodes <159504701+CassieNovaCodes@users.noreply.github.com>
2024-02-10 12:49:08 -0800

    Update afterInstallTouchups.sh

--- afterInstallTouchups.sh
+++ afterInstallTouchups.sh
@@ -1,9 +1,6 @@
-pacman -S xorg xorg-xinit xf86-video-fbdev picom nitrogen lxqt firefox --noconfirm
+sudo pacman -S xorg xorg-xinit xf86-video-fbdev picom nitrogen lxqt firefox --noconfirm
 echo "\n" | pacman -S base-devel --noconfirm
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git
-echo "\n" | makepkg -si
-cd ..
+cd ~/
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 head -n -5 ~/.xinitrc > ~/.xinitrc.tmp && mv ~/.xinitrc.tmp ~/.xinitrc
 echo "$lines_to_add" >> ~/.xinitrc
