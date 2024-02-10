ef795e708ded87077d2352884b8b5b62588b60fd
CassieNovaCodes <159504701+CassieNovaCodes@users.noreply.github.com>
2024-02-10 10:59:45 -0800

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -1,3 +1,7 @@
+    read -p "What Will The Root Password Be? " root_password
+    read -p "What Will The Primary Username Be? " username
+    read -p "What Will The Password For $username Be? " user_password
+    read -p "What Will The Host Name Be? (Name of The Computer) " hostname
     clear
     echo "Generating Locales"
     hwclock --systohc
@@ -9,3 +13,20 @@
     clear
     echo "Setting Passwords"
     echo -e "$root_password\n$root_password" | passwd
+    sudo useradd -m -G wheel,audio,video,optical,storage "$username"
+    echo -e "$user_password\n$user_password" | sudo passwd "$username"
+    clear
+    echo "Adding Hosts"
+    echo "127.0.0.1    localhost" | sudo tee -a /etc/hosts >/dev/null
+    echo "::1          localhost" | sudo tee -a /etc/hosts >/dev/null
+    echo "127.0.1.1    ${hostname}.localdomain ${hostname}" | sudo tee -a /etc/hosts >/dev/null
+    clear
+    echo "Adding Privlages"
+    pacman -S sudo --noconfirm
+    #ADDD PRIVLAGES TO USER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    clear
+    echo "Installing Grub"
+    pacman -S grub efibootmgr dosfstools os-prober mtools --noconfirm
+    mkdir /boot/EFI
+    mount /dev/sda1 /boot/EFI
+    grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
