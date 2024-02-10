34c0f61d03f0bc1b2f140892aa711128755f73f5
CassieNovaCodes <159504701+CassieNovaCodes@users.noreply.github.com>
2024-02-10 12:52:09 -0800

    Update afterInstallTouchups.sh

--- afterInstallTouchups.sh
+++ afterInstallTouchups.sh
@@ -3,7 +3,7 @@ echo "\n" | pacman -S base-devel --noconfirm
 cd ~/
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 head -n -5 ~/.xinitrc > ~/.xinitrc.tmp && mv ~/.xinitrc.tmp ~/.xinitrc
-echo "$lines_to_add" >> ~/.xinitrc
 lines_to_add="picom&\n
 nitrogen&\n
 exec startlxqt"
+echo "$lines_to_add" >> ~/.xinitrc
